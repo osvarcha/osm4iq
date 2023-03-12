50e9de67bc081562616c41bc20477cc834898b17
OSCAR JEAN FRANCO VARGAS-CHAVEZ <osvarcha@hotmail.com>
2023-03-12 18:13:15 -0500

    Change name files, funciones -> udf

--- osm4iq.py
+++ osm4iq.py
@@ -2,8 +2,8 @@ import xlwings as xw
 
 # Funciones a importar
 from package.macros.main import main
-from package.funciones.demo import hello, hola_mundo
-from package.funciones.num import double_sum
+from package.udf.demo import hello
+from package.udf.num import double_sum
 
 if __name__ == "__main__":
     xw.Book("osm4iq.xlsm").set_mock_caller()
